4044bed476d0f8b055f52bd78c3652c17d17b6e1
sanghaokai <sanghaokai@jd.com>
2018-08-29 09:56:58 +0800

    ...

---
+++ src/base/baseClass.java
@@ -0,0 +1,10 @@
+package base;
+
+public class baseClass {
+
+	public static void main(String[] args) {
+		// TODO 自动生成的方法存根
+		System.out.println("hello java");
+	}
+
+}
--- src/leetcode/最大子序和.java
+++ src/leetcode/最大子序和.java
@@ -10,16 +10,18 @@ public class 最大子序和 {
         for(int i=1;i<nums.length;i++) {
         	dp[i] = Math.max(dp[i-1] + nums[i], nums[i]);
         }
+//        Integer max = IntStream.range(0, dp.length).reduce((i,j) -> dp[i] > dp[j] ? dp[i] : dp[j]).getAsInt();
+        int max = Integer.MIN_VALUE;
         for(int i:dp) {
-        	System.out.print(i+" ");
+        	if (i>max)
+        		max = i; 
         }
-        Integer max = IntStream.range(0, dp.length).reduce((i,j) -> dp[i] > dp[j] ? dp[i] : dp[j]).getAsInt();
         return max;
     }
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] nums = {-2,-3,-1};
+		int[] nums = {-2, 1, -3, 4, -1 ,2, 1, -5, 4};
 		int result = maxSubArray(nums);
 		System.out.println(result);
 //		int[] a = {8, 6, 3, 10};			
